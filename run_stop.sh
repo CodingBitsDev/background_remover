85436e223cf16a40ff3e6ca59bdbb18fa4bb0ee6
Renji <lprenji94@gmail.com>
2022-02-06 19:46:03 +0100

    modify startStop script

--- run_stop.sh
+++ run_stop.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 file=/tmp/bgremoval
+SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
 if [[ -s $file ]] ; then
  process=$(cat $file)
  kill $process
  rm $file
 else
-    python3 ./bgRemoval.py & PID=$!;
+    python3 $SCRIPTPATH/bgRemoval.py & PID=$!;
     echo $PID > $file;
 fi
